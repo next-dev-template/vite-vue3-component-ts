9149b3b79e5ec2a543e1fe0ef921e016de064027
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2022-05-15 12:57:59 +0800

    ci: vite config

--- vite.config.preview.ts
+++ vite.config.preview.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import pkg from './package.json';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import pkg from "./package.json";
+import { getName } from "pkg-get";
+
+const name = getName(pkg.name);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${pkg.name}/`,
-  plugins: [ vue() ],
+  base: `/${name}/`,
+  plugins: [vue()],
 });
