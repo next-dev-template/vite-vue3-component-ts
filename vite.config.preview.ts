308ec475fb3bca8be787f364dac534bf80e84955
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2022-08-02 10:56:03 +0800

    chore: vscode配置

--- vite.config.preview.ts
+++ vite.config.preview.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import pkg from "./package.json";
-import { getName } from "pkg-get";
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+import { getName } from 'pkg-get';
+import base from './vite.config.base';
 
 const name = getName(pkg.name);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  ...base,
   base: `/${name}/`,
-  plugins: [vue()],
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import pkg from './package.json';
 import { getName } from 'pkg-get';
+import base from './vite.config.base';
 
 const name = getName(pkg.name);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ vue() ],
+  ...base,
   build: {
     outDir: './lib',
     lib: {
